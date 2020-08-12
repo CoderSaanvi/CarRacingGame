1815437293d9f82f7c0a330515a96085cbaa5cf7
CoderSaanvi <65832874+CoderSaanvi@users.noreply.github.com>
2020-08-12 14:57:33 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+var car1Img,car2Img,car3Img,car4Img,track;
+
+function preload(){
+  // ./ takes the absolute the value from the root folder/ from the beginning  The name of ./ is absolute part of a file
+  // you need relative parts when the files are presented or stored in the same folder
+  track = loadImage("./images/track.jpg");
+  car1Img = loadImage("./images/car1.png");
+  car2Img = loadImage("./images/car2.png");
+  car3Img = loadImage("./images/car3.png");
+  car4Img = loadImage("./images/car4.png");
+}
+
+
+function setup(){
+  //displayWidth or height takes the entire windows width and height, -30 takes some of the marjin
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
